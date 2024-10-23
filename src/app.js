3b33509fb77fc6d55f68a74c43e62c73e8f1d226
Yuvraj <redhuyuvraj01@gmail.com>
2024-10-23 09:09:10 +0530

    multiple route

--- src/app.js
+++ src/app.js
@@ -5,21 +5,49 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user" , (req,res) => {
-    res.send("getting data from the user:");
-})
+// app.get("/user/ab*c" , (req,res) => {
+//     res.send("getting data from the user:");
+// })
 
-app.post("/user" , (req , res) => {
-    res.get("post the data from the user");
-})
+// app.post("/user" , (req , res) => {
+//     res.get("post the data from the user");
+// })
 
-app.delete("/user" , (req,res) =>{
-    res.delete("delete the data from the database");
-})
+// app.delete("/user" , (req,res) =>{
+//     res.delete("delete the data from the database");
+// })
 
-app.use("/user" , (req , res)=> {
-    res.send("mai hi hu");
+// app.use("/user" , (req , res)=> {
+//     res.send("mai hi hu");
+// });
+
+
+
+
+
+// Rout handlers
+
+app.use("/user" ,
+   (req , res , next) =>{
+        console.log("Handling the route 1 ");
+        // res.send("1st response");
+        next();
+},
+   
+
+(req,res) =>{
+    console.log("Handling the route 2");
+    res.send("2nd response");
 });
+
+
+
+
+app.use("/route" ,route 1 , [route 2 , route 3], route 4 , route 5 )
+
+
+
+
 
 
 app.listen(3222 , ()=> {
